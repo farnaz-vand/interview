c554c4b11feeb43e061e897186c1d8c6652bc6f4
Nazxiggvj <nazxiggvj@gmail.com>
2026-06-30 10:42:12 +0330

    feat: implement recursive FormNode for dynamic nested forms

--- src/sections/dashboard/view/dashboard-view.jsx
+++ src/sections/dashboard/view/dashboard-view.jsx
@@ -5,15 +5,24 @@ import { useForm } from 'react-hook-form';
 import { Typography } from '@mui/material';
 
 import { Form } from 'src/components/hook-form';
+import data from '../../../../public/data.json'; 
+
+import FormNode from './form_node';
 
 export default function DashboardView() {
+  console.log('Full JSON Content:', data);
   const methods = useForm();
 
   const { handleSubmit } = methods;
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
-  });
+  const onSubmit = methods.handleSubmit((formValues) => {
+  const cleaned = JSON.parse(
+    JSON.stringify(formValues, (key, value) => (value === undefined ? undefined : value))
+  );
+
+  console.log('Final Clean JSON:', cleaned);
+});
+
 
   return (
     <>
@@ -21,11 +30,9 @@ export default function DashboardView() {
         Welcome to RUNC!
       </Typography>
       <Form methods={methods} onSubmit={onSubmit}>
-        {/*
-      import Field component from src/components/hook-form
-      and use them based on the json file you were given :
-      <Filed.Text ...hook form props />
-      */}
+    <FormNode node={data.appHdr.element} />
+
+    <button type='submit'>submit</button>
       </Form>
     </>
   );
